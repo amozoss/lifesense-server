30346e9f63636dc4bba23a45a6593d136d24bdaa
Dan Willoughby <amozoss@gmail.com>
2014-06-23 01:43:57 -0600

    fix alert error and admin view ability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   before_action :signed_in_user, only: [:edit, :update, :destroy, :show, :index]
   before_action :correct_user,   only: [:edit, :update, :show]
-  before_action :admin_user,     only: [:destroy, :index]
+  before_action :admin_user,     only: [:destroy, :index, :show]
 
 
   def index
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user) 
+      redirect_to(root_url) unless current_user?(@user) || current_user.admin?
     end
 
     def admin_user
